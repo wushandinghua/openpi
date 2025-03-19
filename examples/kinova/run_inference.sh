b423934b5feaf72a96cbb3ad0607dc772c8aefa7
wushandinghua <quebinbin666@163.com>
2025-03-19 15:20:17 +0800

    modify run inference

--- examples/kinova/run_inference.sh
+++ examples/kinova/run_inference.sh
@@ -1,1 +1,7 @@
-python scripts/main.py --remote_host="192.168.3.101" --remote_port=8000 --instruction="Pick up the orange and put it in the drawer."
+
+# uv venv --python 3.10 examples/kinova/.venv
+source examples/kinova/.venv/bin/activate
+source ~/IS_Table/Kinova_End2End_Basic/ros_ws/devel/setup.zsh
+# uv pip install -e packages/openpi-client
+
+python examples/kinova/main.py --args.host "192.168.3.101" --args.port 8000 --args.instruction "Pick up the orange and put it in the drawer."
