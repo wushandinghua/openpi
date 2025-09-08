ad46e6c31bb4d6368885c4694cc8d4188105c39a
wushandinghua <quebinbin666@163.com>
2025-09-08 15:11:47 +0800

    rename dataset of piper

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -837,7 +837,7 @@ _CONFIGS = [
         name="pi0_piper_lora",
         model=pi0.Pi0Config(action_horizon=10, paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"),
         data=LeRobotAirbotDataConfig(
-            repo_id="qbb/pick_rubbish_piper_0906",
+            repo_id="qbb/gongjianghang_task_0907",
             base_config=DataConfig(
                 local_files_only=True,  # Set to True for local-only datasets.
                 prompt_from_task=True,
